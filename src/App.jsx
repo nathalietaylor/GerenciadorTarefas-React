6a0ca0bd5e777f50561e01e2bddee2d0f55cb9bb
Nathalie Taylor <nmtzampieri@gmail.com>
2024-11-04 15:49:37 -0300

    commit inicio tasks.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,50 @@
+import { useState } from "react";
 import AddTask from "./components/addTask";
 import Tasks from "./components/tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "tarefa 1",
+      description: "descrição da tarefa 1",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "tarefa 2",
+      description: "descrição da tarefa 2",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "tarefa 3",
+      description: "descrição da tarefa 3",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id == taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      } else {
+        return task;
+      }
+    });
+
+    setTasks(newTasks);
+  }
+
   return (
-    <div>
-      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
-      <AddTask />
-      <Tasks />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+      </div>
     </div>
   );
 }
