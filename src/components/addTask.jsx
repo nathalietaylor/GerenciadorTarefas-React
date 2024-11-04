e3f2c7fa9dc20990ac4328ac30cbbaffd8b48c68
Nathalie Taylor <nmtzampieri@gmail.com>
2024-11-04 18:51:44 -0300

    commit addTask

--- src/components/addTask.jsx
+++ src/components/addTask.jsx
@@ -1,5 +1,34 @@
-function AddTask() {
-  return <h1>Add Task</h1>;
+import { useState } from "react";
+
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("222");
+  const [description, setDescription] = useState("333");
+
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <input
+        type="text"
+        placeholder="Digite o título da tarefa"
+        className="border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        className="border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button
+        onClick={() => onAddTaskSubmit(title, description)}
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+      >
+        Adicionar
+      </button>
+    </div>
+  );
 }
 
 export default AddTask;
