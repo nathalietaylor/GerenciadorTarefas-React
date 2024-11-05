a38233060dc2ea0a9fc5a41f655f9ba62decf641
Nathalie Taylor <nmtzampieri@gmail.com>
2024-11-05 17:49:19 -0300

    task page

--- pages/TaskPage.jsx
+++ pages/TaskPage.jsx
@@ -1,14 +1,33 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { ChevronRightIcon, ChevronLeftIcon, TrashIcon } from "lucide-react";
 
 function TaskPage() {
-  const [useSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
 
   return (
     <div className="h-screen w-screen bg-slate-500 p-6">
-      <h1>{title}</h1>
-      <p>{description}</p>
+      <div className="w-[500px] mx-auto space-y-4">
+        <div className="flex justify-center relative">
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute left-0 top-0 bottom-0 text-slate-100"
+          >
+            <ChevronLeftIcon />
+          </button>
+
+          <h1 className="text-3xl text-slate-100 font-bold text-center">
+            Detalhes da Tarefa
+          </h1>
+        </div>
+
+        <div className="bg-slate-200 p-4 rounded-md">
+          <h2 className="text-xl font-bold text-slate-600">{title}</h2>
+          <p className="text-slate-600">{description}</p>
+        </div>
+      </div>
     </div>
   );
 }
